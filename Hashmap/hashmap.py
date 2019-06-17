df3a57b976c81076a1d704855dc7a3f21ee3a5ea
Jishnu <jish_r@live.com>
2019-06-17 23:18:51 +1000

    Added basic hash map

---
+++ Hashmap/hashmap.py
@@ -0,0 +1,66 @@
+
+class map:
+
+    def __init__(self, size):
+        self.size = size
+        self.keys = [None] * self.size
+        self.value = [None] * self.size
+
+
+    def put(self,key,value):
+
+        probeIndex = 0
+        hashValue = self.hash_function(key,len(self.keys), probeIndex)
+
+
+        if self.keys[hashValue] == None:
+
+            self.keys[hashValue] = key
+            self.value[hashValue] = value
+
+        elif self.keys[hashValue] == key:
+            self.value[hashValue] = value
+
+        else:
+
+            positionFound = False
+
+            while not  positionFound:
+
+                probeIndex += 1
+                rehash = self.hash_function(key,len(self.keys),probeIndex)
+
+                if self.keys[hashValue] == None:
+                    self.keys[hashValue] = key
+                    self.value[hashValue] = value
+                    positionFound = True
+
+                elif self.keys[hashValue] == key:
+                    self.value[hashValue] = value
+                    positionFound = True
+
+
+    def get(self,key):
+
+        probeIndex = -1
+        found = False
+
+        while found != True and probeIndex < self.size:
+            probeIndex += 1
+            hashValue = self.hash_function(key, len(self.keys), probeIndex)
+
+            if self.keys[hashValue] == None:
+                return False
+
+            elif self.keys[hashValue] == key:
+                return self.value[hashValue]
+
+            else:
+                continue
+
+
+
+    def hash_function(self,key,size, probeIndex):
+
+        return ( (key % size) + probeIndex )
+
